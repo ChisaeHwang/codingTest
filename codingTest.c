13408a474a7c5e9d2aade5397e1b28e4be866f98
ChisaeHwang <cwh73090@naver.com>
2022-04-04 00:51:08 +0900

    codeUp 1085 예제

--- codingTest.c
+++ codingTest.c
@@ -3,17 +3,11 @@
  
 int main(){
  
-    int r,g,b;
-    scanf("%d %d %d",&r, &g, &b);
-    
-    for(int i = 0; i<r; i++){
-        for(int j = 0; j<g; j++){
-            for(int k = 0; k<b; k++ ){
-                printf("%d %d %d\n",i,j,k);
-            }
-        }
-    }
-    printf("%d", r*g*b);
+    float h, b ,c, s, mb = 0;
+
+    scanf("%f %f %f %f", &h, &b, &c, &s);
+    mb = (h * b * c * s) / 8 / 1024 / 1024;
+    printf("%.1f MB", mb);
  
     return 0;
 }
