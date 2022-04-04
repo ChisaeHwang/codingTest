03a97da08e9cc6f9bfb5b8b3b74e393be405c1c6
ChisaeHwang <cwh73090@naver.com>
2022-04-05 00:50:57 +0900

    codeUp 0405 풀이

--- codingTest.c
+++ codingTest.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
- 
- 
-int main(){
- 
-    float h, b ,c, s, mb = 0;
 
-    scanf("%f %f %f %f", &h, &b, &c, &s);
-    mb = (h * b * c * s) / 8 / 1024 / 1024;
-    printf("%.1f MB", mb);
- 
+int main(){
+
+    float w, h, b, mb = 0;
+    scanf("%f %f %f", &w, &h, &b);
+    mb = (w * h * b) / 8 / 1024 / 1024;
+    printf("%.2f MB", mb);
+    
     return 0;
 }
