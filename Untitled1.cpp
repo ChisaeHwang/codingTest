2f26278c93e675903a5cc0d033ed768ef607e424
ChisaeHwang <cwh73090@naver.com>
2022-04-07 05:35:23 +0900

    codeup 1089 예제

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,25 +2,16 @@
 
 int main(){
 
-    int n, a;
+    int a, d, n;
     
-    scanf("%d %d", &n ,&a);
+    scanf("%d %d %d", &a,&d,&n);
     
-    for (int i=1; i<=n; i++){
-	    if(i % 3){
-	        printf("%d ", i);
-		}
+    for(int i=1; i<=n; i++){
+    	a += d;
 	}
 	
- 	for(int j = 1; j <=a; j++){
-        if(j % 5 == 0){
-            continue;
-        }
-        else{
-            printf("%d ",j);
-        }
-    }
-    
+	printf("%d", a);
+	
     return 0;
 }
 
