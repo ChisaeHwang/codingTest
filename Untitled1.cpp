22aacef4c602a4b50e8190dcb27f720e63b7b47a
ChisaeHwang <cwh73090@naver.com>
2022-04-12 15:59:45 +0900

    1091 예제

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,64 +2,19 @@
 
 int main(){
  
-	int a, b, c;
-    
-    printf("국어 영어 수학 점수를 입력하시오: ");
-    scanf("%d %d %d", &a, &b, &c);
-    
-    printf("국어 : %d \n", a);
-    printf("영어 : %d \n", b);
-    printf("수학 : %d \n", c);
-    
-    float sum = a + b + c;   
-    printf("총점 : %.0f \n", sum);
-    
-    float ave = sum / 3;
-    printf("평균 : %.2f \n", ave);
-    
-	int rank = 0; 
-	 
-	if(ave > 100 || ave < 0){
-		printf("잘못된 점수를 입력하셨습니다.");
-			
-		return 0;
-	}else{	
+	long long int a, m, d, n;
 	
-		if(ave >= 90){
-			printf("등급 : A \n");
-			rank = 1;
-		}else if 
-			(ave >= 80){
-				printf("등급 : B \n");
-				rank = 1; 
-		}else if
-			(ave >= 70){
-				printf("등급 : C \n");
-				rank = 2;
-		}else if
-			(ave >= 60){
-				printf("등급 : D \n");
-				rank = 2;
-		}else{
-				printf("등급 : F \n");
-				rank = 3;
-			}
-		
-	    
-	    switch(rank){
-	    	case 1:
-	    		printf("조치사항 : 장학금을 받으세요");
-	    		break;
-	    	case 2:
-	    		printf("조치사항 : 보충 수업을 들으세요");
-	    		break;
-	    	case 3:
-	    		printf("조치사항 : 재수강바랍니다.");
-	    		
-		}
+	scanf("%lld %lld %lld %lld", &a, &m, &d, &n);
+	
+	for(int i=1; i<m; i++){
+		a *= m;
+		a += d;
 	}
+	
+	printf("%lld", a);
 		
     return 0;
 }
+
 
 
