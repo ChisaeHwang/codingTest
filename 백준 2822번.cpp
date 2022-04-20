b9aef6be2b231636b1b26f61cd21990b7997c426
ChisaeHwang <cwh73090@naver.com>
2022-04-20 21:40:59 +0900

    백준 2822번 풀이

--- 백준 2822번.cpp
+++ 백준 2822번.cpp
@@ -1,17 +1,42 @@
-#include <stdio.h>
-#include <algorithm> 
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(){
+	
+        vector<pair <int, int> > v(8);
 
-	int arr[8] = {};
-	
-	for(int i=0; i<8; i++){
-		scanf("%d", &arr[i]);
-	}
-	
-	
+        for (int i = 0; i < 8; i++)
+        {
+        	cin >> v[i].first;
+            v[i].second = i + 1;
+        }
 
-	return 0;
+        sort(v.begin(), v.end(), greater<pair<int, int> >());
+        
+        int sum = 0;
+
+        vector<int> idx;
+
+        for (int i = 0; i < 5; i++)
+        {
+            sum += v[i].first;
+			idx.push_back(v[i].second);
+			
+			cout << sum << "\n";
+        }
+
+        cout << sum << "\n";
+
+        sort(idx.begin(), idx.end());
+
+        for (int i = 0; i < idx.size(); i++)
+                 cout << idx[i] << " ";
+        cout << "\n";
+
+        return 0;
 	
 }
+
+
