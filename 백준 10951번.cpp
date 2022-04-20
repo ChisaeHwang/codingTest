cb422fea65dbeaae1691b916790e71fc89ccbf2a
ChisaeHwang <cwh73090@naver.com>
2022-04-20 23:57:26 +0900

    백준 10951번 eof 관련 문제

---
+++ 백준 10951번.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b;    
+    while (!(cin >> a >> b).eof()) // eof : end-of-file 파일의 끝을 의미함
+    {   
+        cout << a + b << "\n";
+    }
+    
+	return 0; 
+}
