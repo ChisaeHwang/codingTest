429a64725c9654b168b054492db60ba8107f9b85
ChisaeHwang <cwh73090@naver.com>
2022-05-06 14:35:56 +0900

    백준 11053번

--- 백준 11053번.cpp
+++ 백준 11053번.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
+int d[1000];
+int arr[1000];
 int main(){
 	
     ios_base::sync_with_stdio(false);
 	cin.tie(nullptr); 
 	
-	int t;
-	cin >> t;
-	while(t--){
-		
+	int n, ans=0;
+	cin >> n;
+	
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
 	}
-
+	
+	for(int i=0; i<n; i++){
+		d[i] = 1;
+		for(int j=0; j<i; j++){
+			if(arr[j] < arr[i]){ // j < i ÀÌ´Ù  
+				d[i] = max(d[i], d[j] + 1);
+				cout << d[i];
+			}
+		}
+		ans = max(ans,d[i]);
+	}
+	
+	cout << ans;
+	
     return 0;
 }
 
- 
+  
