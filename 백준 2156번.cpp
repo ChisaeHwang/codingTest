8040c48a31334f1c54abe832ec2c3a4865a658c2
ChisaeHwang <cwh73090@naver.com>
2022-05-15 13:25:52 +0900

    백준 1932번

--- 백준 2156번.cpp
+++ 백준 2156번.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+
+int dp[10001];
+int a[10001];
 int main(){
 	
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
+	
+	int n;
+	cin >> n;
+	for (int i=1; i<=n; i++){
+		cin >> a[i];
+	}
+	
+	dp[1] = a[1];
+	dp[2] = a[1] + a[2];
+	
+	for(int i=3; i<=n; i++){
+		dp[i] = max(dp[i-1], max(dp[i-3] + a[i-1] + a[i], dp[i-2] + a[i]));
+		 // 안 마실 때, 한 번 마실 때, 두 번 마실 때 
+	}
+	
+	cout << dp[n];
+	
+	
 	
 	
 	
