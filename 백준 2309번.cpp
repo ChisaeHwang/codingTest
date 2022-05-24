0647d7c1737c5d8b43c11df17f9a157076760820
ChisaeHwang <cwh73090@naver.com>
2022-05-24 17:18:32 +0900

    백준 3085번

--- 백준 2309번.cpp
+++ 백준 2309번.cpp
@@ -8,20 +8,18 @@ int main()
 	int sum=0;
 	for (int i = 0; i < 9; i++) {
 		cin >> arr[i];
-		sum += arr[i];
+		sum += arr[i]; // 9명 키의 합 
 	}
 
 	sort(arr, arr + 9);
 	for (int i = 0; i < 8; i++) {
 		for (int j = 1; j < 9; j++) {
-
-			if (sum - arr[i] - arr[j] == 100) {
-
+			if (sum - arr[i] - arr[j] == 100) { // 7명의 합 
 				for (int k = 0; k < 9; k++) {
 					if (k == i || k == j) continue;
 					cout << arr[k] << '\n';
 				}
-				return 0;
+				return 0; // 가능한 정답이 여러가지일 경우 하나만 출력해야한다. 
 			}
 		}
 	}
---
+++ 백준 3085번.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// 브루트 포스 : 모든 경우의 수를 다 해보는 알고리즘 ? 
+
+using namespace std;
+
+int check(vector<string> &a){
+	int n= a.size();
+	int ans = 1;
+	for(int i=0; i<n; i++){
+		int cnt = 1;
+		for(int j=1; j<n; j++){
+			if(a[i][j] == a[i][j-1]){
+				cnt += 1;
+			} else {
+				cnt = 1;
+			}
+			if(ans < cnt) ans = cnt;
+		}
+		
+		cnt = 1;
+		
+		for(int j=1; j<n; j++){
+			if(a[j][i] == a[j-1][i]){
+				cnt += 1;
+			} else {
+				cnt = 1;
+			}
+			if(ans < cnt) ans = cnt;
+		}
+	}
+	
+	return ans;
+}
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n;
+	cin >> n;
+	vector<string> a(n);
+	for(int i=0; i<n; i++){
+		cin >> a[i];
+	}
+	
+	int ans = 0;
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			if(j+1 < n){ // 오른쪽으로  
+				swap(a[i][j], a[i][j+1]);
+				int temp = check(a);
+				if(ans < temp) ans = temp;
+				swap(a[i][j], a[i][j+1]);
+			}
+			if(i+1 < n){ // 아래로 
+				swap(a[i][j], a[i+1][j]);
+				int temp = check(a);
+				if(ans < temp) ans = temp;
+				swap(a[i][j], a[i+1][j]);
+			}
+		}
+	}
+	
+	cout << ans << '\n'; 
+	
+	
+	return 0;
+}
