426543a681c3d45f7a4d89ec91da85746f7c6b02
ChisaeHwang <cwh73090@naver.com>
2022-06-19 19:06:20 +0900

    백준 2146 다리 만들기 (더 빠른 코드)

---
+++ 백준 2146번 다리 만들기(더 빠른 코드).cpp
@@ -0,0 +1,88 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+int a[100][100];
+int g[100][100];
+int d[100][100];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            if (a[i][j] == 1 && g[i][j] == 0) {
+                g[i][j] = ++cnt;
+                queue<pair<int,int>> q;
+                q.push(make_pair(i,j));
+                while (!q.empty()) {
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+                    for (int k=0; k<4; k++) {
+                        int nx = x+dx[k];
+                        int ny = y+dy[k];
+                        if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                            if (a[nx][ny] == 1 && g[nx][ny] == 0) {
+                                g[nx][ny] = cnt;
+                                q.push(make_pair(nx,ny));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // BFS 한 번으로 해결이 가능한 문제이다 
+    queue<pair<int,int>> q;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            d[i][j] = -1;
+            if (a[i][j] == 1) { 
+                q.push(make_pair(i,j)); // l번 섬이 아닌 모든 섬들을 다 q에 넣음 
+                d[i][j] = 0;
+            }
+        }
+    }
+    while (!q.empty()) { // BFS로 거기로부터 모든 거리 구하기 
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int k=0; k<4; k++) {
+            int nx = x+dx[k];
+            int ny = y+dy[k];
+            if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                if (d[nx][ny] == -1) {
+                    d[nx][ny] = d[x][y] + 1;
+                    g[nx][ny] = g[x][y]; // 섬을 확장하는 방식으로 다리를 만드므로
+										 // 바다도 그룹번호 지정이 가능함 
+                    q.push(make_pair(nx,ny));
+                }
+            }
+        }
+    }
+    int ans = -1;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            for (int k=0; k<4; k++) { // (i, j) - (x, y)
+                int x = i+dx[k]; // 인접한 i x 
+                int y = j+dy[k]; // 인접한 j y
+                if (0 <= x && x < n && 0 <= y && y < n) {
+                    if (g[i][j] != g[x][y]) { // 그룹 번호 다를 때만 
+                        if (ans == -1 || ans > d[i][j] + d[x][y]) { // 바다에 만들어지는 섬거리의 최솟값 
+                            ans = d[i][j] + d[x][y];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    printf("%d\n",ans);
+    return 0;
--- 백준 2146번 다리 만들기.cpp
+++ 백준 2146번 다리 만들기.cpp
@@ -1,10 +1,83 @@
-#include <iostream>
+#include <cstdio>
 #include <queue>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
-
-int main(){
-	
+int a[100][100];
+int g[100][100];
+int d[100][100];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int main() {
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            scanf("%d",&a[i][j]); // 입력받기 
+        }
+    }
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            if (a[i][j] == 1 && g[i][j] == 0) { // 섬이고 그룹번호 없으면 
+                g[i][j] = ++cnt; // 그룹 번호 만들어주기 
+                queue<pair<int,int>> q;
+                q.push(make_pair(i,j));
+                while (!q.empty()) { // 모든점 BFS로 방문하기 
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
+                    for (int k=0; k<4; k++) {
+                        int nx = x+dx[k];
+                        int ny = y+dy[k];
+                        if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                            if (a[nx][ny] == 1 && g[nx][ny] == 0) {
+                                g[nx][ny] = cnt; // 그룹번호 같은 값으로 만들어주기 
+                                q.push(make_pair(nx,ny));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    int ans = -1; // l번 섬과 거리 구하기 가능 
+    for (int l=1; l<=cnt; l++) {
+        queue<pair<int,int>> q;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                d[i][j] = -1; // 거리 -1로 모든 점 초기화 
+                if (g[i][j] == l) { // 그룹번호 l이랑 같으면 l번 섬임 
+                    q.push(make_pair(i,j)); // l번 섬에 포함된 모든칸의 번호 q에 넣기 
+                    d[i][j] = 0; // 거리 0 
+                }
+            }
+        }
+        while (!q.empty()) { // 시작점 구했으르몰 BFS로 거리 구하기 
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for (int k=0; k<4; k++) {
+                int nx = x+dx[k];
+                int ny = y+dy[k];
+                if (0 <= nx && nx < n && 0 <= ny && ny < n) {
+                    if (d[nx][ny] == -1) {
+                        d[nx][ny] = d[x][y] + 1;
+                        q.push(make_pair(nx,ny));
+                    }
+                }
+            }
+        }
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                if (a[i][j] == 1 && g[i][j] != l) { // 섬인데 그룹번호 l번 아니면 
+                    if (ans == -1 || d[i][j]-1 < ans) { // 거기까지 거리에서 1을 뺀 최솟값 
+                        ans = d[i][j]-1;
+                    }
+                }
+            }
+        }
+    }
+    
+    printf("%d\n",ans);
+    
+	return 0;
 }
