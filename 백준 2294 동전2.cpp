99992c667b03e0c4727d04a2f20abeb755cbe7e2
wonjun <cwh73090@naver.com>
2022-06-21 19:38:08 +0900

    백준 2294번 동전 2

---
+++ 백준 2294 동전2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int dp[100001]; // 초기화
+int coin[101];
+int main()
+{
+
+    int n, m;
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> coin[i];
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        dp[i] = 100001;
+    }
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = coin[i]; j <= m; j++)
+        {
+            dp[j] = min(dp[j], dp[j - coin[i]] + 1);
+        }
+    }
+
+    if (dp[m] == 100001)
+        cout << -1 << '\n';
+    else
+        cout << dp[m] << '\n';
+
+    return 0;
+}
