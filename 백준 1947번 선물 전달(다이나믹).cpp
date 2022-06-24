76d58079c0d6d8d2489112d55c86a41c81302e3d
ChisaeHwang <cwh73090@naver.com>
2022-06-24 15:07:20 +0900

    백준 1947번 선물 전달(다이나믹)

---
+++ 백준 1947번 선물 전달(다이나믹).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+long long dp[1000001];
+
+int main() {
+	
+	int n;
+	cin >> n;
+	
+	dp[1] = 0;
+	dp[2] = 1;
+	
+	for(int i=3; i<=n; i++){
+		dp[i] = ((i - 1) * (dp[i - 1] + dp[i - 2])) % 1000000000;
+	}
+	
+	cout << dp[n] <<'\n';
+	
+	return 0;
+}
+
--- 백준 2281번 데스노트(다이나믹).cpp
+++ 백준 2281번 데스노트(다이나믹).cpp
@@ -1,9 +1,44 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
+#include <algorithm>
 using namespace std;
 
+int n, m;
+int Name[1001];
+int dp[1001][1001];
+
+int go(int name, int len) { // 뒤에 빈칸 포함 길이
+	if(name >= n) return 0;
+	int result = dp[name][len];
+	if(result != -1){
+		return result;
+	}
+	
+	// 한칸 내려서 쓰는 경우 
+	result = (m - len + 1)*(m - len + 1) + go(name+1, Name[name] + 1);
+	
+	// 같은 줄에 쓰는 경우 
+	if(len + Name[name] <= m){
+		result = min(result, go(name+1, len + Name[name] + 1));
+	}
+	
+	return result;
+}
+ 
+
 int main(){
+		
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> m;
+	for(int i=0; i<n; i++){
+		cin >> Name[i];
+	}
+	
+	memset(dp, -1, sizeof(dp));
+	
+	cout << go(0, 0) << '\n';
 	
 	return 0;
 } 
