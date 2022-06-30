85b9f2d570418a195a0fa9a512dad25aaa0bbfc9
ChisaeHwang <cwh73090@naver.com>
2022-06-30 21:29:30 +0900

    백준 1535번 안녕

--- 백준 1535번 안녕(다이나믹).cpp
+++ 백준 1535번 안녕(다이나믹).cpp
@@ -1,8 +1,38 @@
 #include <iostream>
-#include <string>
+#include <math.h>
+#include <algorithm>
 using namespace std;
 
-int main() {
-	
-	return 0; 
+int n;
+int d[101];
+int lose[21];
+int joy[21];
+
+void dp(){
+    for(int i=1; i<=n; i++){
+        for(int j=100; j>lose[i]; j--){
+            d[j] = max(d[j], d[j-lose[i]] + joy[i]);
+        }
+    }
+    
+    cout << d[100] << "\n";
+}
+
+int main(int argc, const char * argv[]) {
+    // cin,cout 속도향상
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        cin >> lose[i];
+    }
+    
+    for(int i=1; i<=n; i++){
+        cin >> joy[i];
+    }
+    
+    dp();
+    
+    return 0;
 }
