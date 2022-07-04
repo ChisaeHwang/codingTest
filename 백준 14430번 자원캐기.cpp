e042f8d9035e567daeb6007e0ee91c9e4f003a27
ChisaeHwang <cwh73090@naver.com>
2022-07-04 16:16:39 +0900

    백준 14430번 자원캐기

--- 백준 14430번 자원캐기.cpp
+++ 백준 14430번 자원캐기.cpp
@@ -4,8 +4,28 @@
 
 using namespace std;
 
+int dp[301][301];
+int arr[301][301];
 
 int main(){
+
+	int n, m;
+	cin >> n >> m;
+	
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			cin >> arr[i][j];
+		}
+	}
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=m; j++){
+			dp[i][j] = max(dp[i][j], dp[i-1][j] + arr[i-1][j-1]);
+			dp[i][j] = max(dp[i][j], dp[i][j-1] + arr[i-1][j-1]);
+		}
+	}
+	
+	cout << dp[n][m] <<'\n';
 	
 	return 0;  
 }
