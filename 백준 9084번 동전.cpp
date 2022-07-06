d1333b479048cb936268afa0e933d21d70266429
ChisaeHwang <cwh73090@naver.com>
2022-07-06 13:37:08 +0900

    백준 9084번 동전

--- 백준 9084번 동전.cpp
+++ 백준 9084번 동전.cpp
@@ -9,6 +9,30 @@ int coin[25];
 
 int main() {
 	
+	int t;
+	cin >> t; 
+	while(t--){
+		int n, m;
+		cin >> n;
+		for(int i = 0; i < n; i++){
+			cin >> coin[i];
+		}
+		cin >> m;
+		
+		for(int i= 1 ; i < m+1; i++){
+			dp[i] = 0;
+		}
+		dp[0] = 1;
+		
+		for(int i = 0; i < n; i++){
+			for(int j = coin[i]; j <= m; j++){
+				dp[j] += dp[j - coin[i]];
+			}
+		}
+		
+		cout << dp[m] << '\n';
+	}
+	
 	
 	
 	return 0;
