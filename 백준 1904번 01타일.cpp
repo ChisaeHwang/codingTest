a1ac13696173194a8b1fbaf9251084736329d1e2
ChisaeHwang <cwh73090@naver.com>
2022-07-12 16:19:42 +0900

    백준 4811번 알약, 1904번 01타일

--- 백준 1904번 01타일.cpp
+++ 백준 1904번 01타일.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <algorithm>
 
+#define MOD 15746
+
+int dp[1000001];
+
 using namespace std;
 
 int main() {
+	
+	int n;
+	cin >> n;
+	
+	dp[1] = 1;
+	dp[2] = 2;
+	
+	for(int i=3; i<=n; i++){
+		dp[i] = (dp[i-1] + dp[i-2]) % MOD;
+	}
+	
+	cout << dp[n] << '\n';
 	
 	return 0;
 } 
---
+++ 백준 4811번 알약.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+long long dp[31][31]; 
+
+// 온전한 알약 W개, 반쪽짜리 알약 H개 
+
+int main() {
+
+	for(int i = 0; i < 31; i++){
+		dp[0][i] = 1;
+	} 
+	
+	while(true){
+		cin >> n;
+		if(n == 0) return 0;
+		
+		for(int w = 1; w < n; w++){
+			for(int h = 0; h < n; h++){
+				if(h == 0){
+					dp[w][h] = dp[w-1][1];
+				} else {
+					dp[w][h] = dp[w-1][h+1] + dp[w][h-1];
+				}
+			}
+		}
+		
+		// W : n - 1개, H : 1개의 값을 구하면 됨 
+		cout << dp[n-1][1] << '\n';
+	}
+	
+	return 0;
+}
