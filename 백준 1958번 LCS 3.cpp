9a66d3104320d208f6abac87f37b2718f04b39d0
ChisaeHwang <cwh73090@naver.com>
2022-07-13 16:11:36 +0900

    백준 2839번 설탕 배달

--- 백준 1958번 LCS 3.cpp
+++ 백준 1958번 LCS 3.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <algorithm>
+#include <string> 
 
 using namespace std;
 
+int dp[101][101][101];
+
 int main() {
+	
+	string n1, n2, n3;
+	cin >> n1 >> n2 >> n3;
+	
+	int f = n1.length(), s = n2.length(), t = n3.length();
+	
+	for(int i = 1; i <= f; i++){
+		for(int j = 1; j <= s; j++){
+			for(int k = 1; k <= t; k++){
+				if (n1[i-1] == n2[j-1] && n2[j-1] == n3[k-1]){
+					dp[i][j][k] = dp[i-1][j-1][k-1] + 1;
+				} else {
+					dp[i][j][k] = max(dp[i-1][j][k], max(dp[i][j-1][k], dp[i][j][k-1]));
+				}
+			}
+		}
+	}
+	
+	cout << dp[f][s][t] << '\n';
 	
 	return 0; 
 } 
--- 백준 4811번 알약.cpp
+++ 백준 4811번 알약.cpp
@@ -4,9 +4,7 @@
 using namespace std;
 
 int n;
-long long dp[31][31]; 
-
-// 온전한 알약 W개, 반쪽짜리 알약 H개 
+long long dp[31][31];   // 온전한 알약 W개, 반쪽짜리 알약 H개 
 
 int main() {
 
@@ -20,7 +18,7 @@ int main() {
 		
 		for(int w = 1; w < n; w++){
 			for(int h = 0; h < n; h++){
-				if(h == 0){
+				if(h == 0){ // 반쪽 알약이 없을 경우 
 					dp[w][h] = dp[w-1][1];
 				} else {
 					dp[w][h] = dp[w-1][h+1] + dp[w][h-1];
