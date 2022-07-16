1d6f53cec933f2125bf60e26cc51552e5639c00e
ChisaeHwang <cwh73090@naver.com>
2022-07-16 16:18:07 +0900

    백준 11058번 크리보드

--- 백준 11058번 크리보드.cpp
+++ 백준 11058번 크리보드.cpp
@@ -1,7 +1,28 @@
+#include <iostream>
 #include <algorithm>
 #include <string> 
 
+using namespace std;
+
+long long dp[101];
+
 int main() {
+	
+	int n;
+	cin >> n;
+	
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 3;
+	dp[4] = 4;
+	dp[5] = 5;
+	
+	for(int i = 6; i <= n; i++){
+		dp[i] = max({dp[i - 1] + 1, dp[i - 3] * 2, dp[i - 4] * 3, dp[i - 5] * 4});
+	}
+	
+	cout << dp[n] << '\n';
 	
 	return 0;
 }
