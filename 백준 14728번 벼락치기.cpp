b10a41238a7e3d5c55f46be7155eba33d40a0eb3
ChisaeHwang <cwh73090@naver.com>
2022-07-23 17:07:29 +0900

    백준 14728번 벼락치기

---
+++ 백준 14728번 벼락치기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string> 
+
+using namespace std;
+
+int dp[10001]; 
+
+int main() {
+	
+	int n, t;
+	cin >> n >> t;
+	for(int i = 0; i < n; i++){
+		int k, s;
+		cin >> k >> s;
+		for(int j = t; j >= k; j--){
+			dp[j] = max(dp[j], dp[j - k] + s);
+		} 
+	} 
+	
+	cout << dp[t] << '\n';
+	
+	return 0; 
+}
