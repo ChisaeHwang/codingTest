41e89935dce557261869fd3fab95a5b141089637
ChisaeHwang <cwh73090@naver.com>
2022-08-06 14:53:13 +0900

    백준 3052번 유전자

---
+++ 백준 2306번 유전자.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string> 
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+string s;
+int dp[500][500];
+
+int main() {
+	
+	return 0; 
+} 
