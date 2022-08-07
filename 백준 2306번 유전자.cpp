4e0adde7f250cf64915f89224e1ad48ff19421c0
ChisaeHwang <cwh73090@naver.com>
2022-08-07 10:21:55 +0900

    백준 2306번 유전자

--- 백준 2306번 유전자.cpp
+++ 백준 2306번 유전자.cpp
@@ -9,7 +9,35 @@ using namespace std;
 string s;
 int dp[500][500];
 
+int go(int l, int r) {
+	
+	if (l >= r) return 0;
+	
+	int& result = dp[l][r];
+	if(result != -1) return result;
+	
+	int ans = -1;
+	
+	for(int k = l; k < r; ++k){
+		ans = max(ans, go(l, k) + go(k + 1, r));
+	}
+	
+	if((s[l] == 'a' && s[r] == 't')||(s[l] == 'g' && s[r] == 'c')){
+		ans = max(ans, go(l + 1, r - 1) + 2);
+	}
+	
+	return result = ans;
+	
+}
+
 int main() {
+	
+	
+	memset(dp, -1, sizeof(dp));
+	
+	cin >> s;
+	
+	cout << go(0, s.length() -1) << '\n';
 	
 	return 0; 
 } 
