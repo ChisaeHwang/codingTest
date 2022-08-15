84333ed2a8d7dca3dfa0e19b6ebcc28e17f23f38
ChisaeHwang <cwh73090@naver.com>
2022-08-15 13:16:56 +0900

    나이순 정렬

--- 백준 2133번.cpp
+++ 백준 2133번.cpp
@@ -3,8 +3,10 @@
 
 using namespace std;
 
-
 int D[31]= {1, 0, 3};
+
+dp[n] = dp[n - 1] + dp[n - 2]
+
 int main(){
     
     ios_base::sync_with_stdio(false);
@@ -14,17 +16,18 @@ int main(){
     cin >> n;
 
     if(n%2==0){
-        for(int i=4; i<=n; i+=2){
-    		D[i] = 3*D[i-2];
-    		for(int j=4; j<=i; j+=2){
-    			if(j%2==0){
-        			D[i] += 2*D[i-j];		
+        for(int i = 4; i <= n; i += 2){
+    		D[i] = 3*D[i - 2];
+    		for(int j = 4; j <= i; j += 2){
+    			if(j % 2 == 0){
+        			D[i] += 2*D[i - j];		
 				}
 			}
-		}	
+		}
 	}
 	
 	cout << D[n];
-    
+	
     return 0;
+    
 }
