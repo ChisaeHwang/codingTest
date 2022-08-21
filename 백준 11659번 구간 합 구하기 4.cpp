88a6add11bd697ec410f101b1e2c9ebb8c6b8a1f
ChisaeHwang <cwh73090@naver.com>
2022-08-21 16:33:12 +0900

    백준 11659번 구간 합 구하기 4

---
+++ 백준 11659번 구간 합 구하기 4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[100001];
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);  
+	cin.tie(NULL);
+	
+	int n, m;
+	
+	cin >> n >> m;
+	
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+	}
+	
+	for(int i = 1; i <= n; i++){
+		arr[i] += arr[i - 1];
+		cout << arr[i - 1] << " ";
+	}
+	
+	for(int i = 0; i < m; i++){
+		int a, b;
+		cin >> a >> b;
+		
+		cout << arr[b - 1] - arr[a - 2] << '\n';
+	}
+	
+	
+	return 0; 
+}
