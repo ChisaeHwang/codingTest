3625163468170b197df95b4833464cea65635f06
ChisaeHwang <cwh73090@naver.com>
2022-09-05 15:17:51 +0900

    백준 1085번 직사각형에서 탈출

---
+++ 백준 1085번 직사각형에서 탈출.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	int x, y, w, h;
+	int result;
+	cin >> x >> y >> w >> h;
+
+	result = x;
+	
+	if (w - x < result)
+	{
+		result = w - x;	
+	}
+		
+	if (y < result)
+	{
+		result = y;
+	}
+		
+	if (h - y < result)
+	{
+		result = h - y;
+	}
+	
+	return 0;
+}
