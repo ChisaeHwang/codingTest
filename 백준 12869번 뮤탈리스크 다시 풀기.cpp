218ee3dfaef621004bb0a2bf1457ea6bd06d5eff
ChisaeHwang <cwh73090@naver.com>
2022-12-20 02:29:16 +0900

    beakjoon test

--- 백준 12869번 뮤탈리스크 다시 풀기.cpp
+++ 백준 12869번 뮤탈리스크 다시 풀기.cpp
@@ -3,9 +3,9 @@
  using namespace std;
  
  int visited[61][61][61];
- int n, a[3];
+ int n, arr[3];
  
- int arr[6][3] = {
+ int atk[6][3] = { // 경우의 수는 6가지 있다 3 x 2 (조합) 
  	{9, 3, 1},
  	{9, 1, 3},
  	{3, 1, 9},
@@ -15,13 +15,33 @@
  };
  
  
- struct A {
+ struct SCV { // tuple 대신 struct 사용 
  	int a, b, c;
  };
  
  
- int solve(int a, int b, int c) {
+ int bfs(int a, int b, int c) {
+ 	queue<SCV> q;
+ 	visited[a][b][c] = 1; // 나중에 빼주어야 하는 값이다 
+ 	q.push({a, b, c});
  	
+ 	while(q.size()) {
+ 	    int a = q.front().a; 
+        int b = q.front().b; 
+        int c = q.front().c; 
+        q.pop();
+        if(visited[0][0][0]) break; 
+        for(int i = 0; i < 6; i++){
+            int na = max(0, a - atk[i][0]);
+            int nb = max(0, b - atk[i][1]); 
+            int nc = max(0, c - atk[i][2]); 
+            if(visited[na][nb][nc]) continue;
+            visited[na][nb][nc] = visited[a][b][c] + 1;  
+            q.push({na, nb, nc}); 
+        }
+	}
+	
+	return visited[0][0][0] - 1;
  }
   
  
@@ -30,11 +50,13 @@
 	cin >> n;
 	
 	for (int i = 0; i < n; i++){
-		cin >> a[i];
+		cin >> arr[i];
 	}
 	
+	cout << bfs(arr[0], arr[1], arr[2]) << '\n';
+
+
 	
-
-
+	return 0;
 
  }
