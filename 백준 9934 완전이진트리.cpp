56ddc6e4e75f411ce2549285574cec889c205c34
ChisaeHwang <cwh73090@naver.com>
2022-12-22 20:57:52 +0900

    tee

--- 백준 9934 완전이진트리.cpp
+++ 백준 9934 완전이진트리.cpp
@@ -2,9 +2,48 @@
 
 using namespace std;
 
+int n, _end;
+vector<int> ret[14];
+int arr[1021];
+
+// inOrder, postOrder, preOrder는 외워주는게 좋다 
+
+void inOrder(int s, int e, int Lv) {
+	if (s > e) return;
+	if (s == e) {
+		ret[Lv].push_back(arr[e]); // s가 들어가도 상관없음 
+		return;
+	}
+	
+	int mid = (s + e) / 2; // s + e는 무조건 홀수가 나와서 mid값을 얻을 수 있다
+	ret[Lv].push_back(arr[mid]);
+	
+	// 범위 나누기 왼쪽탐색 
+	inOrder(s, mid - 1, Lv + 1);
+	// 범위 나누기 오른쪽 탐색 
+	inOrder(mid + 1, e, Lv + 1);
+	
+	return; 
+} 
+
+
 int main() {
 	
+	cin >> n;
+	_end = (int)pow(2, n) - 1;
 	
+	for (int i = 0; i < _end; i++){
+		cin >> arr[i];
+	}
+	
+	inOrder(0, _end, 1);
+	
+	for (int i = 1; i <= n; i++){
+		for (int j : ret[i]) {
+			cout << j << " ";
+		}
+		cout << '\n';
+	}
 	
 	
 	return 0;
