3b16035800c34323cf8f11fac379469f44f5ca76
ChisaeHwang <cwh73090@naver.com>
2023-02-06 18:06:30 +0900

    1783 병든 나이트

--- soma/백준 10610번 30.cpp
+++ soma/백준 10610번 30.cpp
@@ -2,15 +2,34 @@
 
 using namespace std;
 
-int n; 
+int n, sum;
+vector<int> v;
 
 int main() {
 	
-	cin >> n;
+	string s;
 	
-	
-	
-	 
+	cin >> s;
+
+    sort(s.begin(), s.end(), greater<int>());
+
+	if (s[s.length() - 1] != '0') {
+		cout << -1;
+	} else {
+		long long sum = 0;
+		
+		for (auto a : s) {
+			sum += a - '0';
+		}
+		
+		if (sum % 3 == 0) {
+			cout << s;
+		} else {
+			cout << -1;
+		}
+		
+		
+	}
 	
 	
 	return 0;
