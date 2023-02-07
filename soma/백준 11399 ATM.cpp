088de334b8372fec746099d5be2f8477f31c493c
ChisaeHwang <cwh73090@naver.com>
2023-02-07 22:19:31 +0900

    11399번 ATM

---
+++ soma/백준 11399 ATM.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[1001];
+int n;
+
+int main() {
+	
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	
+	
+	
+	return 0;
+}
---
+++ soma/백준 1931번 회의실 배정 다시 풀기.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, a, b, ret, cnt;
+vector<pair<int, int>> v; 
+
+int main() {  
+
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		v.push_back({b, a});
+	}
+	
+	sort(v.begin(), v.end());
+	
+	for (int i = 0; i < v.size(); i++) {
+		if (v[i].second >= cnt) { // v[i].second는 각 first 의미 
+			cnt = max(cnt, v[i].first);
+			ret++;
+		}
+	}
+	
+	cout << ret << '\n';
+	
+	
+	
+	
+	return 0;
+}
