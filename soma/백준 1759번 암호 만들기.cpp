e93ab488068be1316a9ed98ea8828902b8b55a98
ChisaeHwang <cwh73090@naver.com>
2023-02-13 04:55:03 +0900

    백준 1759번 암호

---
+++ soma/백준 1759번 암호 만들기.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int l, c;
+
+void solve(int n, vector<char> &al, string pass, int i) {
+	
+}
+ 
+int main() {
+	
+	cin >> l >> c;
+	
+	vector<char> v(c);
+	
+	for (int i = 0; i < c; i++) {
+		cin >> v[i];
+	}
+	
+	
+	
+	return 0;
+}
--- soma/백준 5014번 스타트 링크.cpp
+++ soma/백준 5014번 스타트 링크.cpp
@@ -2,11 +2,48 @@
 
 using namespace std;
 
+int F, S, G, U, D;
+
+int visited[1000001];
+
+int BFS() {
+	
+	queue<int> q;
+	q.push(S);
+	visited[S] = 1;
+	
+	while(q.size()) {
+		
+		int now = q.front();
+		q.pop();
+		
+		if (now == G) {
+			return visited[now] - 1;
+		}
+		
+		int next[2] = {now + U, now - D};
+		
+		for (int i = 0; i < 2; i++) {
+			if (next[i] <= 0 || next[i] > F || visited[next[i]]) continue;
+			visited[next[i]] = visited[now] + 1;
+			q.push(next[i]);
+		}	
+		
+		
+	}	
+	
+	return -1;
+}
+
 int main() {
 	
 	
+	cin >> F >> S >> G >> U >> D;
 	
-	
+	int ret = BFS();
+	if(ret == -1) cout << "use the stairs" << '\n';
+	else cout << ret << '\n';
+	 
 	
 	return 0;
 }
