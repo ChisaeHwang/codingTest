4b37f6cdb500f31cd640ab55cc4a19b025bbee0a
ChisaeHwang <cwh73090@naver.com>
2023-02-14 22:31:40 +0900

    as

--- soma/백준 1759번 암호 만들기.cpp
+++ soma/백준 1759번 암호 만들기.cpp
@@ -4,7 +4,34 @@ using namespace std;
 
 int l, c;
 
+bool check(string &str) {
+	int ja = 0;
+	int mo = 0;
+	
+	for (char x : str) {
+		if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+			mo++;
+		} else {
+			ja++;
+		}
+	}
+	
+	return ja >= 2 && mo >= 1;
+}
+
 void solve(int n, vector<char> &al, string pass, int i) {
+	
+	if (pass.length() == n) {
+		if (check(pass)) {
+			cout << pass << '\n';
+		}
+		return;
+	}
+	
+	if (i >= al.size()) return;
+	solve(n, al, pass + al[i], i + 1);
+	solve(n, al, pass, i + 1);
+	
 	
 }
  
@@ -17,6 +44,10 @@ int main() {
 	for (int i = 0; i < c; i++) {
 		cin >> v[i];
 	}
+	
+	sort(v.begin(), v.end());
+	
+	solve(l, v, "", 0); 
 	
 	
 	
