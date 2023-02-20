b9ec7809c8bffc9245d6a9c83c884cc44a347ce1
ChisaeHwang <cwh73090@naver.com>
2023-02-20 12:11:15 +0900

    백준 11726번 2xN 타일링

--- soma/백준 1261번 알고스팟.cpp
+++ soma/백준 1261번 알고스팟.cpp
@@ -3,19 +3,51 @@
 using namespace std;
 
 int n, m;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int arr[101][101];
+int visited[101][101];
+int INF = 987654321;
 
 int main() {
 	
-	cin >> n >> m;
+	cin >> m >> n;
 	
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
+			scanf("%1d", &arr[i][j]);
+			visited[i][j] = INF;
 		}
 	}
 	
+	queue<pair<int, int>> q;
+	q.push({0 ,0});
+	visited[0][0] = 0;
 	
+	while(q.size()) {
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+		
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+			if (arr[ny][nx] == 1) {
+				if (visited[ny][nx] > visited[y][x] + 1) {
+					visited[ny][nx] = visited[y][x] + 1;
+					q.push({ny, nx});	
+				}
+			} else {
+				if (visited[ny][nx] > visited[y][x]) {
+					visited[ny][nx] = visited[y][x];
+					q.push({ny, nx});
+				}
+			}
+		}
+	}	
+	
+	cout << visited[n - 1][m - 1] << '\n';
 	
 	
 	return 0;
--- soma/백준 1463번 1로 만들기 다시 풀기.cpp
+++ soma/백준 1463번 1로 만들기 다시 풀기.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+int n, cnt = 0;
+int dp[1000001];
+
+int go(int num) {
+	
+	if (num == 1) return 0;
+	if (dp[num] != -1) return dp[num];
+	
+	
+	int ret = go(num - 1) + 1;
+	if(num % 3 == 0) ret = min(ret, go(num / 3) + 1);
+	if(num % 2 == 0) ret = min(ret, go(num / 2) + 1);
+	dp[num] = ret;
+	
+	return ret;
+}
+
 int main() {
 	
+	cin >> n;
 	
+	fill(dp, dp + 1000001, -1);
+	
+	cout << go(n) << '\n';
 	
 	
 	return 0;
