b90ec4da0cbcb153c5547645d84e3a860417a9e6
ChisaeHwang <cwh73090@naver.com>
2023-02-28 16:35:42 +0900

    백준 2294번 동전2

---
+++ soma/백준 2294번 동전 2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[101];
+
+int main() {
+	
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		cio >> arr[i];
+	}
+	
+	
+	
+
+	
+	return 0;
+}
