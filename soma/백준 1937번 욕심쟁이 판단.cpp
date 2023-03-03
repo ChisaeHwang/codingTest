60fe5c02c68aedc49c23d9610e54cad86a3f1199
ChisaeHwang <cwh73090@naver.com>
2023-03-04 01:14:36 +0900

    백준 1937번 욕심쟁이 판단

---
+++ soma/백준 1937번 욕심쟁이 판단.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[101];
+int dp[101];
+
+int main() {
+	
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	for (int i = 0; i < m; i++) {
+		
+	}
+	
+	
+	
+
+	
+	return 0;
+}
