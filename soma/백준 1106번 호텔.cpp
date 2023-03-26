c8383af290d46094a4ddb37fda0429b52b8503f0
ChisaeHwang <cwh73090@naver.com>
2023-03-26 11:50:16 +0900

    t

--- soma/백준 1106번 호텔.cpp
+++ soma/백준 1106번 호텔.cpp
@@ -13,15 +13,16 @@ int main() {
 	for (int i = 1; i <= n; i++) {
 		int a, b;
 		cin >> a >> b;
-		v.push_back({ a,b });
+		v.push_back({a, b});
 	}	
 
 	for (int i = 0; i < v.size(); i++)
 	{
-			for (int k = 1; k <= 100001; k++)
+			for (int k = 1; k <= c; k++)
 			{
-				if(k - v[i].first >= 0)
+				if(k - v[i].first >= 0) // 이걸 체크 안하면 dp에 -인자들이 들어감 
 				dp[k] = max(dp[k], dp[k - v[i].first] + v[i].second);
+				// 각 dp에서 들어갈 수 있는 최댓값 넣기 
 			}
 		
 	}
