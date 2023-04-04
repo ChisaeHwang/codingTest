3a6b7e73381a0e01f92f82d7f6343ace170b4b78
ChisaeHwang <cwh73090@naver.com>
2023-04-04 17:07:46 +0900

    oepn ai

--- soma/백준 14728번 벼락치기.cpp
+++ soma/백준 14728번 벼락치기.cpp
@@ -1,16 +1,27 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, sum;
-int dp[10001];
-
 int main() {
-	
-	cin >> n >> sum;
-	
-	
-	
-	
-	return 0;
-} 
+    int N, T;
+    cin >> N >> T;
+
+    vector<pair<int, int>> topics(N);  // (공부시간, 배점)
+    for (int i = 0; i < N; i++) {
+        cin >> topics[i].first >> topics[i].second;
+    }
+
+    vector<int> dp(T + 1, 0);  // 각 시간에 대한 최대 점수
+    for (int i = 0; i < N; i++) {
+        int study_time = topics[i].first;
+        int score = topics[i].second;
+        for (int j = T; j >= study_time; j--) {
+            dp[j] = max(dp[j], dp[j - study_time] + score);
+        }
+    }
+
+    cout << dp[T] << endl;
+    return 0;
+}
+
