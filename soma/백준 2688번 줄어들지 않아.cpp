d4640c62385cb74a719e2dd19609aedc058adf97
ChisaeHwang <cwh73090@naver.com>
2023-04-12 19:31:59 +0900

    백준 세로 두 줄, 가로로 N개의 칸으로 이루어진 표가 있다. 첫째 줄의 각 칸에는 정수 1, 2, …, N이 차례대로 들어 있고 둘째 줄의 각 칸에는 1이상 N이하인 정수가 들어 있다. 첫째 줄에서 숫자를 적절히 뽑으면, 그 뽑힌 정수들이 이루는 집합과, 뽑힌 정수들의 바로 밑의 둘째 줄에 들어있는 정수들이 이루는 집합이 일치한다. 이러한 조건을 만족시키도록 정수들을 뽑되, 최대로 많이 뽑는 방법을 찾는 프로그램을 작성하시오. 예를 들어, N=7인 경우 아래와 같이 표가 주어졌다고 하자.

---
+++ soma/백준 2688번 줄어들지 않아.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, a;
+int arr[2][101];
+
+int main() {
+	
+	cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		arr[1][i] = i;
+		arr[1][i] = a;
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
