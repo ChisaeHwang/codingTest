4449d71bacc42c7bd68cadce152a0d6182604d0f
ChisaeHwang <cwh73090@naver.com>
2023-05-08 20:44:35 +0900

    백준 9576번 책 나눠주기

--- soma/백준 9576번 책 나눠주기.cpp
+++ soma/백준 9576번 책 나눠주기.cpp
@@ -2,13 +2,47 @@
 
 using namespace std;
 
-priority_queue<pair<int, int>> pq;
-bool book[1001];
+int t;
 
 int main() {
 	
+	cin >> t;
 	
+	while(t--) {
+		
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+		int n, m;
+		
+		cin >> n >> m;
 	
+		for (int i = 0; i < m; i++) {
+			int a, b;
+			cin >> a >> b;
+			pq.push({b, a});
+		}
+		
+		bool book[1001] = {false};
+		int cnt = 0;
+		
+	    while(!pq.empty()){
+			int b = pq.top().first;
+			int a = pq.top().second;
+			pq.pop();
+			
+			int i;
+			for (i = a; i <= b; i++){
+				if(!book[i]) {
+					book[i] = true;
+					cnt++;
+					break;
+				}
+			}
+		}
+		
+		cout << cnt << '\n';
+	
+	}
 	
 	return 0;
 }
+
