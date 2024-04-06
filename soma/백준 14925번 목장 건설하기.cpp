850c446eead6e839e11d434ba5d07c83b1d97806
ChisaeHwang <cwh73090@naver.com>
2024-04-06 09:26:47 +0900

    백준 5569번 출근 경로

---
+++ soma/백준 14925번 목장 건설하기.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[301][301];
+int arr[301][301];
+int n;
+
+int main() {
+	
+	cin >> n;
+	
+
+	return 0;
+}
--- soma/백준 5569번 출근 경로.cpp
+++ soma/백준 5569번 출근 경로.cpp
@@ -1,18 +1,32 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int dp[301][301];
-int arr[301][301];
-int n;
+#define fast ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
+#define ll long long
 
-int main() {
-	
-	cin >> n;
-	
-	
-	
-    cout << ret << '\n'; 
-	
-	return 0;
+// 0 : ²ªÀ» ¼ö ÀÖÀ½. 1 : ²ªÀ» ¼ö ¾øÀ½
+int dp[110][110][2][2]; //dir, 1 or over2
+int ans = 0, w, h, mod = 100000;
+
+int main(){
+    fast;
+    cin >> w >> h;
+
+    for (int i = 2; i <= w; i++) dp[i][1][0][0] = 1;
+    for (int i = 2; i <= h; i++) dp[1][i][1][0] = 1;
+    for (int i = 2; i <= w; i++) {
+        for (int j = 2; j <= h; j++) {
+            dp[i][j][0][0] = (dp[i-1][j][0][0] + dp[i-1][j][0][1]) % mod;
+            dp[i][j][0][1] = dp[i-1][j][1][0];
+            dp[i][j][1][0] = (dp[i][j-1][1][0] + dp[i][j-1][1][1]) % mod;
+            dp[i][j][1][1] = dp[i][j-1][0][0];
+        }
+    }
+    for (int i = 0; i < 2; i++) for (int j = 0; j < 2; j++) ans += dp[w][h][i][j];
+    cout << ans % mod;
+
+
+  return 0;
 }
+
