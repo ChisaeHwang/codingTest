fce1049509c0fef143932bc82ee2b5ce9c8b8ceb
ChisaeHwang <cwh73090@naver.com>
2024-04-10 20:53:46 +0900

    백준

--- soma/백준 17208번 카우버거 알바생.cpp
+++ soma/백준 17208번 카우버거 알바생.cpp
@@ -2,33 +2,38 @@
 
 using namespace std;
 
-int n, m, k;
+int n, m, c;
 
-int dp[301][301]; // 치즈버거, 감자튀김 갯수에 따른 최댓값 
+int dp[101][301][301]; // 주문, 치즈버거, 감자튀김 갯수에 따른 최댓값 
 int chee[301];
 int pota[301];
 
 
 int main() {
 	
-	cin >> n >> m >> k;
+	cin >> n >> m >> c;
 	
-	for(int i = 0; i < n; i++) {
+	for(int i = 1; i <= n; i++) {
 		cin >> chee[i] >> pota[i];
 	}
 	
     memset(dp, 0, sizeof(dp));
 	
-	for(int a = 0; a < n; a++) {
+	
+	for(int i = 1; i <= n; i++) {
 		// 주문을 뒤에서부터 처리하여, 같은 주문을 중복 처리하는 것을 방지
-		for(int i = m; i >= chee[a]; i--) {
-			for(int j = k; j >= pota[a]; j--) {
-				dp[i][j] = max(dp[i][j], dp[i - chee[a]][j - pota[a]] + 1);
+		for(int j = 0; j <= m; j++) {
+			for(int k = 0; k <= c; k++) {
+				if(j >= chee[i] && k >= pota[i]){
+				  dp[i][j][k] = max(dp[i - 1][j][k], dp[i - 1][j - chee[i]][k - pota[i]] + 1);	
+				} else {
+				  dp[i][j][k] = dp[i - 1][j][k];
+				}
 			}
 		}
 	}
 	
-	cout << dp[m][k] << '\n';
+	cout << dp[n][m][c] << '\n';
 	
 	
 	return 0;
