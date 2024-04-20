e1838f06c18fffc565e1a753101176ad874a1f5a
ChisaeHwang <cwh73090@naver.com>
2024-04-20 15:59:37 +0900

    백준 1757번 탑다운

--- soma/백준 1757번 달려달려.cpp
+++ soma/백준 1757번 달려달려.cpp
@@ -1,37 +1,51 @@
 #include <bits/stdc++.h>
+#define INF -987654321
+
 using namespace std;
 
 int arr[10001];
-int dp[10001][501][2]; // 시간, 지침지수, 달리는 여부 0 : 쉬는 경우, 1 : 달리는 경우 
-int n, m;
+int dp[10001][501]; // 시간, 지침지수, 달리는 여부 0 : 쉬는 경우, 1 : 달리는 경우 
+int N, M;
+
+int solve(int n, int m) {
+	
+	 if(n == N) {
+        if(m == 0) return 0;
+        return INF;
+    }
+    
+    int& ret = dp[n][m];
+    if(ret != -1) return ret;
+    
+	ret = INF;
+    
+    if(m > 0) {
+        if(n + m <= N) {
+			ret = max(ret, solve(n + m, 0));
+		}
+    }else {
+        ret = max(ret, solve(n + 1,0));
+    }
+ 
+    if(m + 1 <= M) {
+    	ret = max(ret, solve(n + 1,m + 1) + arr[n]);
+	}
+    return ret;
+	
+}
 
 int main() {
 	
-    cin >> n >> m;
+    cin >> N >> M;
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < N; i++) {
         cin >> arr[i];
     }
     
-    dp[0][1][1] = arr[0];  
-
-   for (int i = 1; i < n; i++) {
-        for (int j = 0; j <= m; j++) {
-        	// 안 달리는 경우 
-        	if(j + 1 <= m) {
-        	 dp[i][j][0] = max({dp[i - 1][j][0], dp[i - 1][j + 1][1], dp[i - 1][j + 1][0]});	
-			} 
-			
-			// 달리는 경우 
-            if(j > 0){ 
-                dp[i][j][1] = dp[i - 1][j - 1][1] + arr[i];
-                if(j == 1) dp[i][j][1] = max({dp[i][j][1], dp[i - 1][0][0] + arr[i]});
-            }
-        }
-    }	
+     memset(dp, -1 ,sizeof(dp));
     
-    cout << dp[n - 1][0][0] << '\n';
-
+    cout << solve(0, 0) << '\n';
+    
     return 0;
 }
 
